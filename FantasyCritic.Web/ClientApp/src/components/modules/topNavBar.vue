cf48a23d58bb55c61706768e69ec0aefe3ddc48b
AndrewKralovec <akrala@yahoo.com>
2020-09-06 17:34:13 -0500

    Add labels for the empty links in the top nav

--- FantasyCritic.Web/ClientApp/src/components/modules/topNavBar.vue
+++ FantasyCritic.Web/ClientApp/src/components/modules/topNavBar.vue
@@ -49,17 +49,17 @@
         <div class="my-2 my-lg-0">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link top-nav-link brand-nav" href="https://twitter.com/fantasy_critic" target="_blank">
+              <a class="nav-link top-nav-link brand-nav" href="https://twitter.com/fantasy_critic" target="_blank" aria-label="fantasy critic twitter account">
                 <font-awesome-icon :icon="['fab', 'twitter-square']" size="lg" class="topnav-icon twitter-icon" />
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link top-nav-link brand-nav" href="https://www.reddit.com/r/fantasycritic/" target="_blank">
+              <a class="nav-link top-nav-link brand-nav" href="https://www.reddit.com/r/fantasycritic/" target="_blank" aria-label="fantasy critic reddit page">
                 <font-awesome-icon :icon="['fab', 'reddit-square']" size="lg" class="topnav-icon reddit-icon" />
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link top-nav-link brand-nav" href="https://discord.gg/dNa7DD3" target="_blank">
+              <a class="nav-link top-nav-link brand-nav" href="https://discord.gg/dNa7DD3" target="_blank" aria-label="fantasy critic discord server">
                 <font-awesome-icon :icon="['fab', 'discord']" size="lg" class="topnav-icon discord-icon" />
               </a>
             </li>
